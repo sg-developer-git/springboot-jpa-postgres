ed42cbce8e8dcc06a5db73a05271cd20216a5a3c
Sandip <contact.mail.sandip@gmail.com>
2021-05-17 22:07:22 +0530

    project init

---
+++ codebase/src/main/java/com/sg/poc/postgres/PostgresPocApplication.java
@@ -0,0 +1,13 @@
+package com.sg.poc.postgres;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PostgresPocApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PostgresPocApplication.class, args);
+	}
+
+}
